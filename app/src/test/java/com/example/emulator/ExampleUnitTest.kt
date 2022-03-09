96abf35b3a860caf47e03578720716a01d09fc65
sakisr <sakisr.git@gmail.com>
2022-03-09 22:29:45 +0200

    Added performCalculation() test

--- app/src/test/java/com/example/emulator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/emulator/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.emulator
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -17,19 +18,19 @@ import org.junit.Assert.*
 }*/
 class GameboyBinaryCalculationTest() {
     @Test
-    fun convertToBits_isCorrect() {
+    fun gameboy_test_convertToBits() {
         val gb = GameBoy()
         val result1 : IntArray = intArrayOf(0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0)
-        assertEquals(gb.convertToBits(0x01, 0x02), result1)
+        val result2 : IntArray = intArrayOf(1,1,0,0,0,0,1,0,1,1,0,1,0,1,1,1)
+        assert(gb.convertToBits(0x01, 0x02).contentEquals(result1))
+        assert(gb.convertToBits(0xc2, 0xd7).contentEquals(result2))
     }
     @Test
-    fun Gameboy_CheckCarry() {
+    fun gameboy_test_performCalculation() {
         val gb = GameBoy()
-        assertEquals(gb.performCalculation(15, 15, "add"), true)
-    }
-
-    @Test
-    fun checkCarry_isCorrect () {
-        //assertEquals(checkCarry())
+        val result1 : IntArray = intArrayOf(0,0,0,0,0,0,1,1)
+        val result2 : IntArray = intArrayOf(1,1,1,0,0,0,0,1)
+        assert(gb.performCalculation(0x01, 0x02, "add").contentEquals(result1))
+        assert(gb.performCalculation(0x1a, 0xc7, "add").contentEquals(result2))
     }
 }
