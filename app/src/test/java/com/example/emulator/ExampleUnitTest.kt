b2b760fb7355af866f83d1f67489429d0e0046db
sakisr <sakisr.git@gmail.com>
2022-03-16 21:44:11 +0200

    Fixed tests

--- app/src/test/java/com/example/emulator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/emulator/ExampleUnitTest.kt
@@ -8,16 +8,20 @@ class GameboyBinaryCalculationTest() {
         val gb = GameBoy()
         val result1 : IntArray = intArrayOf(0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0)
         val result2 : IntArray = intArrayOf(1,1,0,0,0,0,1,0,1,1,0,1,0,1,1,1)
+        val result3 : IntArray = intArrayOf(1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1)
         assert(gb.convertToBits(0x01, 0x02).contentEquals(result1))
         assert(gb.convertToBits(0xc2, 0xd7).contentEquals(result2))
+        assert(gb.convertToBits(0x1fe3, 0x1fe3).contentEquals(result3))
     }
     @Test
     fun gameboy_test_performCalculation() {
         val gb = GameBoy()
         val result1 : IntArray = intArrayOf(0,0,0,0,0,0,1,1)
         val result2 : IntArray = intArrayOf(1,1,1,0,0,0,0,1)
+        val result3 : IntArray = intArrayOf(1,1,1,1,1,1,1,0)
         assert(gb.performCalculation(0x01, 0x02, "add").contentEquals(result1))
         assert(gb.performCalculation(0x1a, 0xc7, "add").contentEquals(result2))
+        assert(gb.performCalculation(0xff, 0xff, "add").contentEquals(result3))
     }
     @Test
     fun gameboy_test_binaryToInteger() {
