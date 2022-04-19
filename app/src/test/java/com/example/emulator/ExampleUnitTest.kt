31851990eb10fe061fa8ee934763572c8bf76577
sakisr <sakisr.git@gmail.com>
2022-04-19 20:15:42 +0300

    Added more ADC calculation tests

--- app/src/test/java/com/example/emulator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/emulator/ExampleUnitTest.kt
@@ -20,23 +20,28 @@ class GameboyBinaryCalculationTest() {
         val result1add : IntArray = intArrayOf(0,0,0,0,0,0,1,1)
         val result2add : IntArray = intArrayOf(1,1,1,0,0,0,0,1)
         val result3add : IntArray = intArrayOf(1,1,1,1,1,1,1,0)
-        // Subtraction
-        val result1sub : IntArray = intArrayOf(0,0,0,0,0,0,1,1)
-        val result2sub : IntArray = intArrayOf(0,1,0,1,0,0,1,1)
-        val result3sub : IntArray = intArrayOf(0,0,0,0,0,0,0,0)
-        // Addition with carry
-        val result1adc : IntArray = intArrayOf(1,1,1,0,0,0,1,0)
 
         assert(gb.performCalculation(0x01, 0x02, "add").contentEquals(result1add))
         assert(gb.performCalculation(0x1a, 0xc7, "add").contentEquals(result2add))
         assert(gb.performCalculation(0xff, 0xff, "add").contentEquals(result3add))
 
-        assert(gb.performCalculation(0x04, 0x01, "sub").contentEquals(result1sub))
-        assert(gb.performCalculation(0x1a, 0xc7, "sub").contentEquals(result2sub))
-        assert(gb.performCalculation(0xff, 0xff, "sub").contentEquals(result3sub))
+        // Addition with carry
+        val result1adc : IntArray = intArrayOf(1,1,1,0,0,0,1,0)
+        val result2adc : IntArray = intArrayOf(1,1,1,1,0,0,0,0)
 
         gb.setFlag('C', 1)
         assert(gb.performCalculation(0x1a, 0xc7, "adc").contentEquals(result1adc))
+        gb.setFlag('C', 1)
+        assert(gb.performCalculation(0xf0, 0xff, "adc").contentEquals(result2adc))
+
+        // Subtraction
+        val result1sub : IntArray = intArrayOf(0,0,0,0,0,0,1,1)
+        val result2sub : IntArray = intArrayOf(0,1,0,1,0,0,1,1)
+        val result3sub : IntArray = intArrayOf(0,0,0,0,0,0,0,0)
+
+        assert(gb.performCalculation(0x04, 0x01, "sub").contentEquals(result1sub))
+        assert(gb.performCalculation(0x1a, 0xc7, "sub").contentEquals(result2sub))
+        assert(gb.performCalculation(0xff, 0xff, "sub").contentEquals(result3sub))
 
     }
     @Test
