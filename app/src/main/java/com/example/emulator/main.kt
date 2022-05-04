41288890c6f0a2b44b507c8f1f8c4b61ed00222f
sakisr <sakisr.git@gmail.com>
2022-05-04 22:02:14 +0300

    Added test roms loading

--- app/src/main/java/com/example/emulator/main.kt
+++ app/src/main/java/com/example/emulator/main.kt
@@ -2,12 +2,15 @@ package com.example.emulator
 
 fun main() {
     val gb = GameBoy()
-    gb.loadRom("app/src/main/assets/Pokemon Red.gb")
+    //gb.loadRom("app/src/main/assets/Pokemon Red.gb")
+    gb.loadRom("app/src/main/assets/gb-test-roms-master/cpu_instrs/cpu_instrs.gb")
     //gb.printOpcodesToFile()
     //gb.printMemoryToConsole()
     //gb.printMemoryToFile()
     while(gb.haltflag == 0){
         gb.fetch()
         gb.decode()
+        //readLine()
+        //gb.printOutputToFile()
     }
 }
