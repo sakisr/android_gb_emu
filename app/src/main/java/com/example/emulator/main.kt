31d202fadb9d932d8fef0bdfa6d368c30432626f
sakisr <sakisr.git@gmail.com>
2022-06-14 12:05:37 +0300

    Added MainActivity

--- app/src/main/java/com/example/emulator/main.kt
+++ app/src/main/java/com/example/emulator/main.kt
@@ -1,5 +1,20 @@
 package com.example.emulator
+/*
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        val myCanvas = CanvasView(this)
+
+        setContentView(myCanvas)
+    }
+}
+
+ */
 fun main() {
     val gb = GameBoy()
     //gb.loadRom("app/src/main/assets/Pokemon Red.gb")
